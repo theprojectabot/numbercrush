9c2884c853f8d547fff57b7db78ba8d824ff83ea
Benji Brown <brown.benj@gmail.com>
2013-01-05 18:22:09 -0800

    changed bg quality to 9.99

--- NumberSmash/assets.lua
+++ NumberSmash/assets.lua
@@ -4,5 +4,5 @@ IconAssets = group{quality=9.95}
 Icon = image{"icon.png"}
 
 GameAssets = group{}
-Background = image{"bg0.PNG", quality = 10}
+Background = image{"bg0.PNG", quality = 9.99}
 Font = image{"font-8x16.png", pinned=true, width=8, height=16}
